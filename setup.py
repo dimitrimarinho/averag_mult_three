6d00e759340035ccb80593254159c337974b9344
dimitrimarinho <dimitri.santana.marinho@gmail.com>
2022-04-10 15:05:27 -0300

    Corrigindo setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     name="averag_mult_three",
     version="0.0.1",
     author="Dimitri Marinho",
-    author_email="my_email",
+    author_email="dimitri.santana.marinho@gmail.com",
     description="Testando a criação de pacote em Python",
     long_description=page_description,
     long_description_content_type="text/markdown",
